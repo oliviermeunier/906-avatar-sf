11beae6ea59649c303b94da641bb3d611bf47bb2
Olivier Meunier <oli.meunier@gmail.com>
2021-08-31 18:14:06 +0200

    AbstractController + Bootstrap

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,28 +4,32 @@ namespace App\Controller;
 
 use Twig\Environment;
 use App\Avatar\AvatarSVGFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
+    const DEFAULT_COLOR_AMOUNT = 2;
+    const DEFAULT_AVATAR_SIZE = 4;
+
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request, Environment $twig, AvatarSVGFactory $avatarSVGFactory)
+    public function index(Request $request, AvatarSVGFactory $avatarSVGFactory)
     {
         // Initialisation des paramètres de l'avatar
-        $colorAmount = 2;
-        $avatarSize = 4;
+        $colorAmount = $request->request->get('color-amount') ?? self::DEFAULT_COLOR_AMOUNT;
+        $avatarSize = $request->request->get('avatar-size') ?? self::DEFAULT_AVATAR_SIZE;
 
         // Création d'un avatar SVG
         $svg = $avatarSVGFactory->createRandomAvatar($avatarSize, $colorAmount);
 
-        $html = $twig->render('home.html.twig', [
-            'svg' => $svg
+        return $this->render('home.html.twig', [
+            'svg' => $svg,
+            'colorAmount' => $colorAmount,
+            'avatarSize' => $avatarSize
         ]);
-
-        return new Response($html);
     }
 }
